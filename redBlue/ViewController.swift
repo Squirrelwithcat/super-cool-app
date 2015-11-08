29e3b959c5853b6c37dcc43d2d5cfb2bc9a5a8c7
Peri And Quinn <periandquinn@imac-in-the-study.local>
2015-11-07 20:41:44 -0500

    Initial Commit

---
+++ redBlue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  redBlue
+//
+//  Created by Peri And Quinn on 2015-11-07.
+//  Copyright © 2015 Peri And Quinn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
