463c8a42ca45e1724d0dd9912d8533fc46dffe0c
Peri And Quinn <periandquinn@iMac-in-the-Study.local>
2015-11-10 13:51:18 -0500

    adding files

--- redBlue/ViewController.swift
+++ redBlue/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var redFish: UIImageView!
 
+    @IBOutlet weak var blueFish: UIImageView!
+    @IBOutlet weak var blueButton: UIButton!
+    @IBOutlet weak var redButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func redButtonPress(sender: AnyObject) {
+        blueFish.hidden = false
+        redFish.hidden = true
+    }
 
+    @IBAction func blueButtonPress(sender: AnyObject) {
+        blueFish.hidden = true
+        redFish.hidden = false
+    }
 }
 
